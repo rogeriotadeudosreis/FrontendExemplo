3a03a157b2bdc3c453a882a4a7643557041a5411
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-03-09 09:25:22 -0300

    Configuração do Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactCreateComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
